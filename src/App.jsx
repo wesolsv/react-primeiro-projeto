7699cf108f2e344e304a4dd6bca2857dd622ab74
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-05-09 16:56:17 -0300

    finalizando aula basica de react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Titulo from "./Titulo"
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
 
 function App(){
   return (
-  <div>
-    <Titulo cor="red"/>
-    <Titulo nome="Alex"/>
-    <Titulo />
-  </div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/sobre" element={<Sobre/>} />
+          <Route path="/contato" element={<Contato/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/pages/Menu.jsx
@@ -0,0 +1,11 @@
+function Menu(){
+    return(
+        <div>
+            <a href="/">Home</a>&nbsp;&nbsp;&nbsp;
+            <a href="/sobre">Sobre</a>&nbsp;&nbsp;&nbsp;
+            <a href="/contato">Contato</a> <br />
+        </div>
+    )
+}
+
+export default Menu
